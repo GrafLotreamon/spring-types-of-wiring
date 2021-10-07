49b0f613bb5ead20cd02a83ce20ad4bc7f94a292
GrafLotreamon <Jiy4e3aphbiu>
2021-10-07 20:09:52 +0300

    Задача решена Хасаров 2.1.4

--- src/main/java/koschei/models/Duck5.java
+++ src/main/java/koschei/models/Duck5.java
@@ -1,12 +1,22 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Duck5 {
 
+
+    private Egg6 eqq6;
+
+
+    @Autowired
+    public void setDuck5(Egg6 eqq6) {
+        this.eqq6 = eqq6;
+    }
+
     @Override
     public String toString() {
-        return ", в утке яйцо " + "";
+        return ", в утке яйцо " + eqq6.toString();
     }
 }
